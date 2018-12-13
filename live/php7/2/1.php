f897dd2b984168f7a294a9719e213697c93ffa7f
houdunwang <2300071698@qq.com>
2018-12-13 21:09:18 +0800

    2

---
+++ live/php7/2/1.php
@@ -0,0 +1,31 @@
+<?php
+    // $a=12;
+    // $Shop
+    // $shop
+    // $shopGoods
+    // $a = 'houduren.com';
+    // $a = 98;
+    //人到中年
+    // echo $a;
+    // var_dump($a);
+    // $a = 1;
+    // $b = &$a;
+    // $a = 2;
+    // echo $b;
+    // var_dump($_GET['page']);
+    echo(isset($_GET['page'])?$_GET['page']:1).'page';
+
+    // define('WEB_NAME','houdunren');
+    // define('WEB_NAME','houdunren');
+    // echo WEB_NAME;
+?>
+<hr>
+<a href="?page=2">page 2</a>
+<!-- <script>
+$a=1;
+a=1;
+$=3;
+jquery
+
+$()
+</script> -->
