c4bdbc591e75929e2e43507f2c3260057b068fe7
houdunren.com <2300071698@qq.com>
2019-06-29 16:40:41 +0800

    error

---
+++ video/php/error/1.php
@@ -0,0 +1,10 @@
+<?php
+// echo 1;
+// echo 2
+// show();
+// function show()
+// {
+//     echo 'show';
+// }
+// echo 1;
+// require 'houdunren';
---
+++ video/php/error/2.php
@@ -0,0 +1,6 @@
+<?php
+// error_reporting(~E_WARNING & ~E_COMPILE_ERROR);
+// error_reporting(E_ALL & ~E_NOTICE);
+// require 'a';
+error_reporting(0);
+echo $a;
