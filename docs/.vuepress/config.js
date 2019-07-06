3963318ca0c7bde63474c603f35210a4552a857a
houdunren.com <2300071698@qq.com>
2019-07-06 19:41:51 +0800

    netlify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,33 +10,90 @@ module.exports = {
         repoLabel: '查看源码',
         editLinks: true,
         editLinkText: '帮助我们改善此页面！',
-        sidebarDepth: 1,
-        displayAllHeaders: true,
-        sidebar: 'auto',
-        nav: [
-            {
+        nav: [{
                 text: '文档列表',
-                items: [
-                    { text: 'PHP', link: '/php/1' }
+                items: [{
+                        text: 'PHP',
+                        link: '/php/1 前言',
+                        desc: "系统学习PHP"
+                    },
+                    {
+                        text: 'Linux',
+                        link: '/linux/ubuntu',
+                        desc: "高效使用Linux"
+                    }
                 ]
             },
             {
                 text: '在线学习',
-                items: [
-                    { text: '在线教育', link: 'https://www.houdunren.com' },
-                    { text: '系统学习', link: 'https://www.houdunren.com/edu/front/system?sid=1&mid=2' },
-                    { text: '实战课程', link: 'https://www.houdunren.com/edu/front/lesson?sid=1&mid=2' },
-                    { text: '最近更新', link: 'https://www.houdunren.com/edu/front/video?sid=1&mid=2' },
-                    { text: '话题讨论', link: 'https://www.houdunren.com/edu/front/topic' },
-                    { text: '签到打卡', link: 'https://www.houdunren.com/edu/front/sign' },
+                items: [{
+                        text: '在线教育',
+                        link: 'https://www.houdunren.com'
+                    },
+                    {
+                        text: '系统学习',
+                        link: 'https://www.houdunren.com/edu/front/system?sid=1&mid=2'
+                    },
+                    {
+                        text: '实战课程',
+                        link: 'https://www.houdunren.com/edu/front/lesson?sid=1&mid=2'
+                    },
+                    {
+                        text: '最近更新',
+                        link: 'https://www.houdunren.com/edu/front/video?sid=1&mid=2'
+                    },
+                    {
+                        text: '话题讨论',
+                        link: 'https://www.houdunren.com/edu/front/topic'
+                    },
+                    {
+                        text: '签到打卡',
+                        link: 'https://www.houdunren.com/edu/front/sign'
+                    },
                 ]
             },
-            { text: '八点直播', link: 'https://www.houdunren.com/edu/front/live' }
+            {
+                text: '八点直播',
+                link: 'https://www.houdunren.com/edu/front/live'
+            }
         ],
-        lastUpdated: 'Last Updated'
-        //     sidebar: [
-        //         ['/php/1 前言', '前言'],
-        //         ['/php/2 基础知识', '基础知识']
-        //     ]
+        lastUpdated: 'Last Updated',
+        sidebarDepth: 2,
+        displayAllHeaders: false,
+        // sidebar: 'auto',
+        // sidebar: [
+        //     ['/php/php', 'PHP'],
+        //     ['/linux/ubuntu', 'ubuntu'],
+        // ],
+        // sidebar: [{
+        //         title: 'PHP',
+        //         collapsable: false,
+        //         children: [
+        //             ['/php/1 前言', '1 前言'],
+        //             ['/php/2 基础知识', '2 基础知识'],
+        //             // '/php/1 前言',
+        //             // '/php/2 基础知识',
+        //             // '/php/3 函数编程'
+        //         ]
+        //     },
+        //     {
+        //         title: 'Linux',
+        //         children: [
+        //             ['/linux/ubuntu', 'ubuntu'],
+        //             ['/linux/zsh', 'zsh'],
+        //         ]
+        //     }
+        // ],
+        sidebar: {
+            '/php/': [
+                ['/php/1 前言', '1 前言'],
+                ['/php/2 基础知识', '2 基础知识'],
+            ],
+
+            '/linux/': [
+                ['/linux/ubuntu', 'ubuntu'],
+                ['/linux/zsh', 'zsh'],
+            ],
+        }
     }
 }
