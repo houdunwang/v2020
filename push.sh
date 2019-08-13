1f4589907a8b9a2a856847737d352536e52e7935
houdunren.com <2300071698@qq.com>
2019-08-13 22:12:04 +0800

    更新课程代码

--- push.sh
+++ push.sh
@@ -4,19 +4,20 @@
 set -e
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
 # git init
 git add -A
-git commit -m 'netlify'
+git commit -m '更新课程代码'
 git push 
 git push gitee
+git push 
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
