acd6e80c416f1df4c7e0ef636c7cdc70adbe78bf
houdunren.com <2300071698@qq.com>
2019-11-18 01:09:40 +0800

    course code

---
+++ video/js/function/4.1.js
@@ -0,0 +1,18 @@
+// (function(window) {
+//   function hd() {
+//     console.log("4.1.js-hd");
+//   }
+//   function show() {
+//     console.log("4.1.js-show");
+//   }
+//   window.js1 = { hd, show };
+// })(window);
+// {
+//   let hd = function() {
+//     console.log("4.1.js-hd");
+//   };
+//   let show = function() {
+//     console.log("4.1.js-show");
+//   };
+//   window.js1 = { hd, show };
+// }
---
+++ video/js/function/4.2.js
@@ -0,0 +1,9 @@
+(function(window) {
+  function hd() {
+    console.log("4.2.js-hd");
+  }
+  function show() {
+    console.log("4.2.js-show");
+  }
+  window.js2 = { hd, show };
+})(window);
